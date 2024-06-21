63de6efb5331d81bc4980a5649de02959ea62666
Caroline Mimeault <carolinemimeault@gmail.com>
2024-06-20 23:06:24 -0400

    adding quarto file in vscode

--- scripts/main.py
+++ scripts/main.py
@@ -32,7 +32,7 @@ print(survey_languages)
 
 # ------------------------------------------------------------------------------
 # Bar plot of survey languages
-fig = px.bar(survey_languages, x="Language", y="Count")
+# figure = px.bar(survey_languages, x="Language", y="Count")
 
 # ------------------------------------------------------------------------------
 # app layout
@@ -42,21 +42,70 @@ app.layout = html.Div(
             "Ottawa Food Bank - 2024 Neighbour Survey", style={"text-align": "center"}
         ),
         dcc.Dropdown(
-            id="language_dropdown",
+            id="language_selection",
             options=[
                 {"label": "Arabic", "value": "Arabic"},
                 {"label": "English", "value": "English"},
                 {"label": "French", "value": "French"},
                 {"label": "Simplified Chinese", "value": "Simplified Chinese"},
             ],
-            multi=True,
+            multi=False,
+            value="English",
             style={"width": "50%"},
         ),
         html.Div(id="output_container", children=[]),
         html.Br(),
-        dcc.Graph(id="language_plot", figure=fig),
+        dcc.Graph(id="language_plot", figure={}),
     ]
 )
+
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    [
+        Output(component_id="output_container", component_property="children"),
+        Output(component_id="language_plot", component_property="figure"),
+    ],
+    [Input(component_id="language_selection", component_property="value")],
+)
+def update_graph(language_selection):
+    print(language_selection)
+    print(type(language_selection))
+
+    container = "The language chosen by user was: {}".format(language_selection)
+
+    dff = survey_languages.copy()
+    dff = dff[dff["Language"] == language_selection]
+
+    # Plotly Express
+
+    fig = px.bar(
+        data_frame=survey_languages,
+        x="Language",
+        y="Count",
+    )
+
+    # Plotly Graph Objects (GO)
+    # fig = go.Figure(
+    #     data=[go.Choropleth(
+    #         locationmode='USA-states',
+    #         locations=dff['state_code'],
+    #         z=dff["Pct of Colonies Impacted"].astype(float),
+    #         colorscale='Reds',
+    #     )]
+    # )
+    #
+    # fig.update_layout(
+    #     title_text="Bees Affected by Mites in the USA",
+    #     title_xanchor="center",
+    #     title_font=dict(size=24),
+    #     title_x=0.5,
+    #     geo=dict(scope='usa'),
+    # )
+
+    return container, fig
+
 
 # ------------------------------------------------------------------------------
 if __name__ == "__main__":
