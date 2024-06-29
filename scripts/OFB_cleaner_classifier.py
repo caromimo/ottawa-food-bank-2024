f4465a60163520a7517f38dd241d6a877819d730
Caroline Mimeault <carolinemimeault@gmail.com>
2024-06-29 11:19:07 -0400

    renamed file

--- scripts/OFB_cleaner_classifier.py
+++ scripts/OFB_cleaner_classifier.py
@@ -10,17 +10,17 @@ SPDX-License-Identifier: MIT
 import pandas as pd
 
 # import data
-df = pd.read_csv("./data/raw/neighbour_survey_clean-2024-06-14.csv")
+df = pd.read_csv("./data/processed/cleaned_neighbour_survey.csv")
 
-# clean data
-df.q003 = df.q003.str.lower()
-df.q004 = df.q004.str.lower()
-df.q005 = df.q005.str.lower()
-df.q006 = df.q006.str.lower()
-df.q007 = df.q007.str.lower()
-df.q008 = df.q008.str.lower()
-df.q009 = df.q009.str.lower()
-df.q011a = df.q011a.str.lower()
+# # clean data
+# df.q003 = df.q003.str.lower()
+# df.q004 = df.q004.str.lower()
+# df.q005 = df.q005.str.lower()
+# df.q006 = df.q006.str.lower()
+# df.q007 = df.q007.str.lower()
+# df.q008 = df.q008.str.lower()
+# df.q009 = df.q009.str.lower()
+# df.q011a = df.q011a.str.lower()
 
 # saved processed data to csv file
 # df.to_csv("test_clean_data.csv", index=False)
